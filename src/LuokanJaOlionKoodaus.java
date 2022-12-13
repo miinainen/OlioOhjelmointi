982921e96875b886ea5cb740f6947808872f86bb
miinainen <miina.niemitalo@student.hamk.fi>
2022-12-14 01:20:04 +0200

    Initial commit

---
+++ src/LuokanJaOlionKoodaus.java
@@ -0,0 +1,67 @@
+
+public class LuokanJaOlionKoodaus {
+
+	public static void main(String[] args) {
+		// Kun ohjelma käynnistyy, tämä lohko suoritetaan ensin
+		// Muodostetaan Auto-luokasta olio
+		Auto auto = new Auto();
+		auto.merkki = "Audi";
+		auto.malli = "Avant";
+		auto.polttoaine = "Diesel";
+		auto.paino = 2100;
+		System.out.println(auto.merkki + " " + auto.malli);
+		
+		auto.Käynnisty();
+		auto.muutaTiedot("Ford", "Focus", "Bensa", 2300);
+		auto.tulostaTiedot();
+		
+	}
+
+} // Main-luokan lopetus
+
+
+class Auto {
+	// Ominaisuudet
+	String merkki;
+	String malli;
+	String polttoaine;
+	int paino;
+	
+	// Toiminnallisuudet
+	public void Käynnisty()
+	{
+		System.out.println("Käynnistää auton");
+	}
+	
+	public void Sammuta()
+	{
+		System.out.println("Sammuttaa auton");
+	}
+	
+	public void Ajaa()
+	{
+		System.out.println("Ajaa autoa");
+	}
+	
+	public void Jarruta()
+	{
+		System.out.println("Jarruttaa autoa");
+	}
+	public void tulostaTiedot()
+	{
+		System.out.println("Auto");
+		System.out.println("Merkki " + merkki);
+		System.out.println("Malli " + malli);
+		System.out.println("Polttoaine " + polttoaine);
+		System.out.println("Paino " + paino);
+	}
+	// Tietojen muuttamis-metodi
+	public void muutaTiedot(String merkkiNimi, String malliNimi,String polttoaineNimi, int painoLuku)
+	{
+		merkki = merkkiNimi;
+		malli = malliNimi;
+		polttoaine = polttoaineNimi;
+		paino = painoLuku;
+	}
+	
+}
